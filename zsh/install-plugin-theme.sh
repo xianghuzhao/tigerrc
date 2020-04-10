9b078645c8df51d7eb4360e181d78e7f8a95fb87
Xianghu Zhao <xianghuzhao@gmail.com>
2020-04-11 05:23:27 +0800

    Use powerlevel10k

--- zsh/install-plugin-theme.sh
+++ zsh/install-plugin-theme.sh
@@ -5,20 +5,23 @@ cd $(dirname "$0")
 zsh_custom_dir=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
 
 # Install zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions.git ${zsh_custom_dir}/plugins/zsh-autosuggestions
+git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git ${zsh_custom_dir}/plugins/zsh-autosuggestions
 
 # Install zsh-history-substring-search
-git clone https://github.com/zsh-users/zsh-history-substring-search ${zsh_custom_dir}/plugins/zsh-history-substring-search
+git clone --depth=1 https://github.com/zsh-users/zsh-history-substring-search ${zsh_custom_dir}/plugins/zsh-history-substring-search
 
 # Install zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${zsh_custom_dir}/plugins/zsh-syntax-highlighting
+git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ${zsh_custom_dir}/plugins/zsh-syntax-highlighting
 
 # Install my plugin
 cp -r plugins/tiger ${zsh_custom_dir}/plugins
 
 
 # Install powerlevel9k theme
-git clone https://github.com/bhilburn/powerlevel9k.git ${zsh_custom_dir}/themes/powerlevel9k
+#git clone --depth=1 https://github.com/bhilburn/powerlevel9k.git ${zsh_custom_dir}/themes/powerlevel9k
+
+# Install powerlevel10k theme
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${zsh_custom_dir}/themes/powerlevel10k
 
 # Install my theme
 cp themes/tiger.zsh-theme ${zsh_custom_dir}/themes
--- zsh/plugins/tiger/tiger.plugin.zsh
+++ zsh/plugins/tiger/tiger.plugin.zsh
@@ -11,9 +11,17 @@ unsetopt sharehistory
 
 alias ta='tmux attach || tmux'
 
-POWERLEVEL9K_CONTEXT_TEMPLATE="%n@%M"
-POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(context dir vcs newline os_icon)
-POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status time)
-POWERLEVEL9K_TIME_FORMAT='%D{%Y/%m/%d %H:%M:%S}'
+#zsh_current_os(){
+#    local os=$()
+#
+#    local color='%F{yellow}'
+#    echo -n "%{$color%}\uf230  $os%{%f%}" # \uf230 is 
+#}
+
+#POWERLEVEL9K_CUSTOM_CURRENT_OS="zsh_current_os"
+#POWERLEVEL9K_CONTEXT_TEMPLATE="%n@%M"
+#POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(context custom_current_os dir vcs newline os_icon)
+#POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status time)
+#POWERLEVEL9K_TIME_FORMAT='%D{%Y/%m/%d %H:%M:%S}'
 
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=239'
